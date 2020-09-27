88dc7ad6ca722b26ee2bfde8bd3b09bf1eeba59a
Amish Raj <43755573+amishraj@users.noreply.github.com>
2020-09-27 13:19:39 +0530

    Twitter API Service Added, to be used later

---
+++ src/app/twitter.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TwitterService {
+
+  apiKey= 'q5CCLGRTXMX72cvZdOgKcyBEd'
+  apiSecretKey= 'jpAK7bu32yOWTZa5N1K8VaiheoVGi9bmRpj5Leq9r95Bmhqf5P'
+  bearerToken='AAAAAAAAAAAAAAAAAAAAABxbIAEAAAAAXhADzXGqiCf2wVakgw7zuimPXWA%3DabV6gNjnEyJl3Rr0JpU4IXZT3pZtGzgi7AQOEOivHlQqMpHG5k'
+
+  //App ID: 18897692
+
+  constructor() { }
+}
