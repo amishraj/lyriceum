f42bc6ebe22bcff6e32c4b8130342592f5121f4e
Amish Raj <43755573+amishraj@users.noreply.github.com>
2022-05-11 14:40:46 +0530

    lyriceum update

---
+++ src/app/env/variables.ts
@@ -0,0 +1,5 @@
+ export const GENIUS_CREDENTIALS = {
+   client_Id: "53MOVV4Ouu9Ywm6z8HBBnxJH2irRVctC9PbnQRu7VBTKsxLaRm2tdqgZ4Elw-i5t",
+   client_secret: "Z7ajR-d6csy1GNOY7ogq0u41cI38JKruRI7_YM9IeYBJuBoQP7mdNsxxkNu9cjOTDTXeyNnz3XRXfxkltkIIwA",
+   client_access_token: "ReRPLP3vfffuokCowiGO6f3gqbL-_XHd0T0coeT40i_Z5GQi5cXZlNB1iY45ioTg"
+ }
